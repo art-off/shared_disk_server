74d673efa8e706f2e80f50810256ee73b63c1901
root <root@art-off.bpi18-01.ru>
2021-05-06 09:24:57 +0300

    test 2

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from flask import url_for
+import flask
+
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
+from google_auth_oauthlib.flow import Flow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
@@ -12,17 +16,23 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World'
 
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+CLIENT_SECRETS_FILE = 'credentials.json'
+global_state = ''
+
 @app.route('/authorize')
 def authorize():
     # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes=SCOPES)
+    flow = InstalledAppFlow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
+
 
     # The URI created here must exactly match one of the authorized redirect URIs
     # for the OAuth 2.0 client, which you configured in the API Console. If this
     # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
     # error.
-    flow.redirect_uri = flask.url_for('oauth2callback', _external=True)
+    flow.redirect_uri = url_for('oauth2callback', _external=True, _scheme='https')
 
     authorization_url, state = flow.authorization_url(
         # Enable offline access so that you can refresh an access token without
@@ -32,6 +42,45 @@ def authorize():
         include_granted_scopes='true')
 
     # Store the state so the callback can verify the auth server response.
-    flask.session['state'] = state
+    global_state = state
 
     return flask.redirect(authorization_url)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+    # Specify the state when creating the flow in the callback so that it can
+    # # verified in the authorization server response.
+    state = global_state
+
+    flow = Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+
+    flow.redirect_uri = flask.url_for('oauth2callback', _external=True, _scheme='https')
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = flask.request.url.replace('http://', 'https://', 1)
+    flow.fetch_token(authorization_response=authorization_response)
+
+    # Store credentials in the session.
+    # ACTION ITEM: In a production app, you likely want to save these
+    #              credentials in a persistent database instead.
+    credentials = flow.credentials
+    # flask.session['credentials'] = credentials_to_dict(credentials)
+
+    service = build('drive', 'v3', credentials=credentials)
+
+    # Call the Drive v3 API
+    results = service.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    result = ''
+    if not items:
+        result += 'No files found.'
+    else:
+        result += 'Files:<br>'
+        for item in items:
+            result += u'{0} ({1})<br>'.format(item['name'], item['id'])
+
+    return result
